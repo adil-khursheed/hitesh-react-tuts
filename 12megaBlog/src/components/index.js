66e05f7ed50ddcc5d7acd752b059cf1bc3927564
adil-khursheed <adilkhursheed60@gmail.com>
2023-09-30 19:54:40 +0530

    post functionality

--- 12megaBlog/src/components/index.js
+++ 12megaBlog/src/components/index.js
@@ -3,5 +3,25 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button";
+import Input from "./Input";
+import Login from "./Login";
+import PostCard from "./PostCard";
+import Select from "./Select";
+import Signup from "./Signup";
+import RTE from "./RTE";
 
-export { Header, Footer, Container, Logo, LogoutBtn };
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  Button,
+  Input,
+  Login,
+  PostCard,
+  Select,
+  Signup,
+  RTE,
+};
